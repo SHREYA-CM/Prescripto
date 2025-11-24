80dafa4b7170faeb24a067dcfb7cb5260e049385
Shreya <tumhara.email@example.com>
2025-11-24 22:01:09 +0530

    updated mailer

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,24 +1,24 @@
 // backend/utils/mailer.js
-const nodemailer = require('nodemailer');
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// Your API Key
+const resend = new Resend("rnd_ChrjTEvfSoLAbnZEvld9fBMoHPmr");
 
 async function sendMail({ to, subject, text, html }) {
-  const mailOptions = {
-    from: `"Prescripto" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-    html,
-  };
+  try {
+    const response = await resend.emails.send({
+      from: "Prescripto <noreply@resend.dev>",
+      to,
+      subject,
+      html: html || `<p>${text}</p>`,
+    });
 
-  return transporter.sendMail(mailOptions);
+    console.log("Email Sent:", response);
+    return response;
+  } catch (error) {
+    console.error("Email error:", error);
+    throw error;
+  }
 }
 
 module.exports = { sendMail };
